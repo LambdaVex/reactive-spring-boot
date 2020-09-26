b54475fff7fd628b07e88097c013a23d7f0277c6
Ali Denno <aliedenno@gmail.com>
2020-09-26 22:39:30 +0200

    feat: add stock-service
    A Kotlin Rest Service

---
+++ stock-service/src/test/kotlin/com/adenno/demo/stockservice/StockServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.adenno.demo.stockservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
