a9caa3e9d3dd77dc218ddf5e0c6869a1d01afb47
Ali Denno <aliedenno@gmail.com>
2020-09-26 22:55:10 +0200

    feat: add new RestController
    Controller that will serve stock prices when called on
    http://localhost:8080/stocks/DEMO

--- stock-service/src/main/kotlin/com/adenno/demo/stockservice/StockServiceApplication.kt
+++ stock-service/src/main/kotlin/com/adenno/demo/stockservice/StockServiceApplication.kt
@@ -2,6 +2,14 @@ package com.adenno.demo.stockservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import java.time.Duration
+import java.time.LocalDateTime
+import java.util.concurrent.ThreadLocalRandom
 
 @SpringBootApplication
 class StockServiceApplication
@@ -9,3 +17,21 @@ class StockServiceApplication
 fun main(args: Array<String>) {
 	runApplication<StockServiceApplication>(*args)
 }
+
+@RestController
+class RestController {
+	@GetMapping(value = ["/stocks/{symbol}"],
+				produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+	fun prices(@PathVariable symbol: String): Flux<StockPrice> {
+		return Flux.interval(Duration.ofSeconds(1))
+				.map{ StockPrice(symbol, randomStockPrice(), LocalDateTime.now()) }
+	}
+
+	private fun randomStockPrice(): Double {
+		return ThreadLocalRandom.current().nextDouble(100.0);
+	}
+}
+
+data class StockPrice(val symbol: String,
+					  val price: Double,
+					  val time: LocalDateTime)
